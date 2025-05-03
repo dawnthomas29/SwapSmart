12a99a08fc9549046b0e1860608de131f7f2257b
dawnthomas29 <donthomasbinoy814@gmail.com>
2025-05-03 15:53:05 +0530

    Add files via upload

---
+++ Backend/routes/productRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const productController  = require('../controllers/productController');
+
+// Route to add a new product
+router.post('/', productController.addProduct);
+
+// Route to get all products
+router.get('/', productController.getAllProducts);
+
+// Route to get products by userId
+router.get('/user/:userId', productController.getUserProducts);
+
+// Route to delete a product by ID
+router.delete('/:id', productController.deleteProduct);
+
+// Route to update a product by ID
+router.put('/:id', productController.updateProduct);  // New route for updating products
+
+module.exports = router;

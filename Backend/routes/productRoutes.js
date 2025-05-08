df70fc748b3066d076b5312da0446f7563ffff00
dawnthomas29 <donthomasbinoy814@gmail.com>
2025-05-08 11:46:49 +0530

    Update productRoutes.js

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -2,34 +2,24 @@ const express = require('express');
 const router = express.Router();
 const productController  = require('../controllers/productController');
 
-// Borrow Product Route ✅
 router.post('/borrow/:productId', productController.borrowProduct);
 
-// to cancel borrowed item (return item)
 router.post('/cancelBorrow/:productId', productController.cancelBorrow);
 
-// return borrowed items for profile display.
 router.get('/borrowed/:userId', productController.getBorrowedProducts);
 
-// Route to add a new product
 router.post('/', productController.addProduct);
 
-// Route to get all products
 router.get('/', productController.getAllProducts);
 
-// Route to get products by userId
 router.get('/user/:userId', productController.getUserProducts);
 
-// Route to delete a product by ID
 router.delete('/:id', productController.deleteProduct);
 
-// Route to update a product by ID
-router.put('/:id', productController.updateProduct); // New route for updating products
+router.put('/:id', productController.updateProduct); 
 
-// Get products by category
 router.get('/category/:category', productController.getProductsByCategory);
 
-// Search products by name
 router.get('/search', productController.searchProductsByName);
 
 module.exports = router;

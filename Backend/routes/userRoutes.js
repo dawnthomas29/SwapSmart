0979c3da912568d07f53df5a7a72087b979d4ab2
dawnthomas29 <donthomasbinoy814@gmail.com>
2025-05-08 11:47:35 +0530

    Update userRoutes.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -10,28 +10,20 @@ const {
   unblockUser,
   getAdminData,
 } = require('../controllers/userController');
-// Register User
 router.post('/reg', registerUser);
 
-// Login User
 router.post('/log', loginUser);
 
-// Get User Profile
 router.get('/profile/:userId', getUserProfile);
 
-// Update User Profile
 router.put('/profile/:userId', updateUserProfile);
 
-// Fetch all users
-router.get('/', fetchUsers);  // Route for fetching users
+router.get('/', fetchUsers);  
 
-// Block user route
 router.patch('/block/:userId', blockUser);
 
-// Unblock user route
 router.patch('/unblock/:userId', unblockUser); 
 
-// Route for fetching admin data (added this)
 router.get('/admin', getAdminData); 
 
 module.exports = router;
